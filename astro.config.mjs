be2bbf623c602f590c7a7136c5e3416f3f8649fb
Scott Rod <scott@spinlinestudio.com>
2024-06-10 16:06:40 -0400

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://astro-prefetch-test.netlify.app',
+	prefetch: true,
 	integrations: [mdx(), sitemap()],
 });
