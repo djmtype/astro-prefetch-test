f02b24983a714f9ddef429b7b63f33f74302def2
Scott Rod <scott@spinlinestudio.com>
2024-06-13 12:09:26 -0400

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://astro-prefetch-test.netlify.app',
 	// prefetch: true,
-	trailingSlash: 'never',
+	trailingSlash: 'always',
 	prefetch: true,
 	integrations: [mdx(), sitemap()],
 });
